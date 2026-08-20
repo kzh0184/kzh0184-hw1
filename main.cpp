def2f377c640326964ab7c72a3445a6ed46ea93c
Ryan <kzh0184@auburn.edu>
2026-08-20 14:57:51 -0500

    Add course and student name output
    Added course and student name variables with output.

--- main.cpp
+++ main.cpp
@@ -8,4 +8,9 @@ int main()
 	//create a few starter objects
 	//add some values to test out your values methods
 	//print out all the values for each object to validate your code
+	String course = "COMP2710";
+	String studentname = "Ryan H.";
+	cout << course;
+	cout << studentname;
+	return 0;
 }
