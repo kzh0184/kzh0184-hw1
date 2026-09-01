940409ea78246c9db98d1f1c8be942f67985afba
Ryan <kzh0184@auburn.edu>
2026-09-01 16:31:06 +0000

    commit 9/1/2026

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@ int main()
     // Add some values to test out your values methods
     // Print out all the values for each object to validate your code
 
+    Starter_Object ob1;
+    ob1.val1(67);
+    ob1.val2(69);
+    cout << ob1.val1() << endl;
+    cout << ob1.val2() << endl;
     string course = "COMP2710";
     string studentname = "Ryan H.";
 
